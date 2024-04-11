5c46c857e64d4fb38456a0033ca934654a0244e8
hossein <hossein.bajan@gmail.com>
2024-04-11 11:24:37 +0330

    Fix bug

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -8,18 +8,31 @@ import { Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  header1!: HTMLElement;
+  header2!: HTMLElement;
+
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    this.header1 = this.elementRef.nativeElement.querySelector('#header1');
+    this.header2 = this.elementRef.nativeElement.querySelector('#header2');
+
+    if (this.header1 && this.header2) {
+      this.renderer.setStyle(this.header1, 'display', 'block');
+      this.renderer.setStyle(this.header2, 'display', 'none');
+    }
+  }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    const header1 = this.elementRef.nativeElement.querySelector('#header1');
-    const header2 = this.elementRef.nativeElement.querySelector('#header2');
-    if (window.scrollY > 0) {
-      this.renderer.setStyle(header1, 'display', 'none');
-      this.renderer.setStyle(header2, 'display', 'block');
-    } else {
-      this.renderer.setStyle(header1, 'display', 'block');
-      this.renderer.setStyle(header2, 'display', 'none');
+    if (this.header1 && this.header2) {
+      if (window.scrollY > 20) {
+        this.renderer.setStyle(this.header1, 'display', 'none');
+        this.renderer.setStyle(this.header2, 'display', 'block');
+      } else {
+        this.renderer.setStyle(this.header1, 'display', 'block');
+        this.renderer.setStyle(this.header2, 'display', 'none');
+      }
     }
   }
 }
