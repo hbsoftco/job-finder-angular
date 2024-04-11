2fa3c6b5af859ab373d134aa40fc2f61762a9ac9
hossein <hossein.bajan@gmail.com>
2024-04-11 12:49:59 +0330

    Fix router link in header and navigation bar

--- src/app/layouts/bottom-navigation-bar/bottom-navigation-bar.component.ts
+++ src/app/layouts/bottom-navigation-bar/bottom-navigation-bar.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-bottom-navigation-bar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './bottom-navigation-bar.component.html',
-  styleUrl: './bottom-navigation-bar.component.scss'
+  styleUrl: './bottom-navigation-bar.component.scss',
 })
-export class BottomNavigationBarComponent {
-
-}
+export class BottomNavigationBarComponent {}
